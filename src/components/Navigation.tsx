63a3e2b7c20c2e4b352dc410ebdfdf9078bd24a3
Your Name <davish9@gmail.com>
2023-08-11 16:23:46 -0400

    adding farmers page and command palette

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,20 @@ import { cn } from "@/lib/utils";
 import UserBadge from "@/components/UserBadge";
 import Link from "next/link";
 import { FaHome, FaUserEdit, FaMoneyBillWave } from "react-icons/fa";
+import { GiFarmer } from "react-icons/gi";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { Button } from "./ui/button";
 
 interface NavItem {
   name: string;
@@ -26,6 +39,12 @@ const navItems: NavItem[] = [
     onMobile: true,
   },
   {
+    name: "Farmers",
+    href: "/dashboard/farmers",
+    icon: <GiFarmer />,
+    onMobile: true,
+  },
+  {
     name: "My Profile",
     href: "/dashboard/profile",
     icon: <FaUserEdit />,
@@ -35,31 +54,83 @@ const navItems: NavItem[] = [
 
 export default function Navigation({ className }: { className?: string }) {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   return (
-    <nav className="md:w-72 h-fit md:h-full md:flex flex-col bg-slate-100 border-t border-slate-200 md:bg-white md:border-r md:border-slate-100 p-2 md:p-4">
-      <div className="md:mt-10 flex flex-row md:flex-col gap-y-4">
-        {navItems.map((item) => {
-          const active = pathname.endsWith(item.href);
-          return (
-            <Link
-              key={item.name}
-              className={cn(
-                "text-slate-800 text-base font- hover:text-slate-700 hover:bg-slate-100 rounded-xl w-full p-2 flex flex-row gap-x-4",
-                active && "bg-slate-100"
-              )}
-              href={item.href}
-            >
-              <div className="md:mx-0 w-full md:w-fit my-auto text-lg flex">
-                <span className="mx-auto md:mx-0">{item.icon}</span>
-              </div>
-              <h2 className="hidden md:block">{item.name}</h2>
-            </Link>
-          );
-        })}
-      </div>
-      <div className="hidden md:block mt-auto">
-        <UserBadge />
-      </div>
-    </nav>
+    <>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandGroup heading="Suggestions">
+            {navItems.map((item) => {
+              return (
+                <CommandItem
+                  onSelect={() => {
+                    router.push(item.href);
+                    setOpen(false);
+                  }}
+                  key={item.name}
+                  className="text-slate-700 flex flex-row"
+                >
+                  <span className="mr-4 h-4 w-4 my-auto text-slate">
+                    {item.icon}
+                  </span>
+                  <span>{item.name}</span>
+                </CommandItem>
+              );
+            })}
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+      <nav className="md:w-72 h-fit md:h-full md:flex flex-col bg-slate-100 border-t border-slate-200 md:bg-white md:border-r md:border-slate-100 p-2 md:p-4">
+        <Button
+          onClick={() => setOpen(true)}
+          className="flex flex-row group text-slate-500 hover:text-slate-700 transition-all ease-in-out duration-300 shadow-sm hover:shadow-md"
+          variant={"outline"}
+        >
+          <div className="mr-auto">Search...</div>{" "}
+          <div className="ml-auto text-slate-500 text-xs my-auto border border-slate-200 group-hover:borer-slate-300 rounded-md px-1 py-0.5">
+            ⌘K
+          </div>
+        </Button>
+        <div className="md:mt-10 flex flex-row md:flex-col gap-y-4">
+          {navItems.map((item) => {
+            const active = pathname.endsWith(item.href);
+            return (
+              <Link
+                key={item.name}
+                className={cn(
+                  "text-slate-800 text-base font- hover:text-slate-700 hover:bg-slate-100 rounded-xl w-full p-2 flex flex-row gap-x-4",
+                  active && "bg-slate-100",
+                  !item.onMobile && "hidden md:flex"
+                )}
+                href={item.href}
+              >
+                <div className="md:mx-0 w-full md:w-fit my-auto text-lg flex">
+                  <span className="mx-auto md:mx-0">{item.icon}</span>
+                </div>
+                <h2 className="hidden md:block">{item.name}</h2>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="hidden md:block mt-auto">
+          <UserBadge />
+        </div>
+      </nav>
+    </>
   );
 }
